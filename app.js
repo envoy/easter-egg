7f8e0b111300da524e29299b092db8f8c181ff30
jeromechavez <jayrome12@yahoo.com>
2018-10-08 12:36:26 -0700

    Completed implementation the sign-up and the challenge page

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-let keywords = ["Ember", "Ruby", "Rails", "Elixir", "React"];
+let keywords = ["Ember", "Ruby", "Rails", "Elixir", "React", "JavaScript"];
+
+var $input = document.querySelector("input");
+var $signin = document.getElementById('signin');
+var $modal = document.getElementById('modal');
+var $yesButton = document.getElementById('yes');
+var $noButton = document.getElementById('no');
+
+$input.addEventListener("keydown", function(event) {
+  var inputValue = event.target.value;
+  for (var i = 0; i < keywords.length; i++) {
+    if (inputValue.includes(keywords[i])) {
+      $signin.classList.toggle('hide');
+      $modal.classList.toggle('hide');
+    }
+  };
+});
+
+$yesButton.addEventListener("click", function() {
+  window.location.href = 'challenge.html'
+});
+
+$noButton.addEventListener("click", function() {
+  $signin.classList.toggle('hide');
+  $modal.classList.toggle('hide');
+});
+
---
+++ challenge.js
@@ -0,0 +1,47 @@
+const $numbers = document.getElementById('numbers')
+const $submitButton = document.getElementById('submit')
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10)
+};
+
+function compareArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false
+    }
+  }
+  return true
+}
+const array = new Array(getRandomNumber()).fill(0).map(function(x) {
+  return getRandomNumber()
+});
+
+$numbers.innerHTML = array.toString()
+
+let answer = new Set(array.sort())
+let answerArray = [...answer]
+let count = 0
+
+$submitButton.addEventListener('click', () => {
+  count++
+  const userAnswer = document.querySelector("input").value.split(",")
+  for (let i = 0; i < userAnswer.length; i++) {
+    userAnswer[i] = parseInt(userAnswer[i], 10)
+  }
+  let checked = compareArrays(answerArray, userAnswer)
+
+  if (checked) {
+    window.location.href = 'pass.html'
+  }
+  else {
+    window.location.href = 'retry.html'
+  }
+})
+
+
+
+
